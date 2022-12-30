5ae397f7e6c48b3ec55df8fd82b8ebda305707da
tonche92 <tonche92@gmail.com>
2022-12-30 16:40:45 +0300

    controller refactoring class refactoring data moved to config

--- WinPharmaAPI/APIRequest.cs
+++ WinPharmaAPI/APIRequest.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json;
 
 namespace WinPharmaAPI
@@ -7,12 +8,131 @@ namespace WinPharmaAPI
         public APIResult result { get; set; }
         public string status { get; set; }
         public string requestId { get; set; }
+        public APIRequest()
+        {
+            result = new APIResult();
+            status = "";
+            requestId = "";
+        }
+        public APIRequest(APIResult result, string status, string requestId)
+        {
+            this.result = result;
+            this.status = status;
+            this.requestId = requestId;
+        }
+        public APIRequest(int? Skip, int? Take, string? Expand, string? Filter, string? OrderBy, string? OrderDirection, IMemoryCache cache, IConfiguration config)
+        {
+            
+            //объявляем переменную, которая будет ключем в кэше
+            string addToUrl = "";
+            if (Skip != null)
+            {
+                addToUrl += "Skip=" + Skip.ToString();
+            }
+            if (Take != null)
+            {
+                addToUrl += (addToUrl != "" ? "&" : "") + "Take=" + Take.ToString();
+            }
+            if (Expand != null)
+            {
+                addToUrl += (addToUrl != "" ? "&" : "") + "Expand=" + Expand;
+            }
+            if (Filter != null)
+            {
+                addToUrl += (addToUrl != "" ? "&" : "") + "Filter=" + Filter;
+            }
+            if (OrderBy != null)
+            {
+                addToUrl += (addToUrl != "" ? "&" : "") + "OrderBy=" + OrderBy;
+            }
+            if (OrderDirection != null)
+            {
+                addToUrl += (addToUrl != "" ? "&" : "") + "OrderDirection=" + OrderDirection;
+            }
+            var res = new APIRequest();
+            try
+            {
+                var url = config.GetValue<string>(
+                "url");
+                var auth = config.GetValue<string>(
+                "base64Auth");
+                var json = GetJsonFromUrl(url + addToUrl, auth);
+                res = JsonConvert.DeserializeObject<APIRequest>(json);
+                //Если апи ответила нормально, но со статусом Error - берем из кэша
+                if (res != null && res.status == "Error")
+                {
+                    //Если в кэше не найдено - бросаем ошибку
+                    if (!cache.TryGetValue(addToUrl, out res))
+                    {
+                        throw new Exception("API overloaded and value not in cache");
+                    }
+                    //Если найдено - меняем статус
+                    else
+                    {
+                        //не понятно зачем эта проверка, но без нее студия ругается на possible null, не смотря на проверку на 58 строчке.
+                        if (res != null)
+                            res.status = "FromCache";
+                    }
+                }
+                //Добавляем в кэш по ключу и полученному res
+                else
+                {
+                    cache.Set(addToUrl, res);
+                }
+            }
+            //Отлавливаем непредвиденные ошибки, пытаемся взять значение из кэша, если не получается - кидаем ошибку
+            catch
+            {
+                if (!cache.TryGetValue(addToUrl, out res))
+                {
+                    throw new Exception("Error occured and value not in cache");
+                }
+                else
+                {
+                    if (res != null)
+                        res.status = "FromCache";
+                }
+            }
+            if (res != null)
+            {
+                this.result = res.result;
+                this.status = res.status;
+                this.requestId = res.requestId;
+            }
+            else
+            {
+                this.result = new APIResult();
+                this.status = "Error";
+                this.requestId = "Error";
+            }
+        }
+
+        public static string GetJsonFromUrl(string url, string base64EncodedAuthenticationString)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            url);
+
+            request.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var client = new HttpClient();
+
+            var response = client.Send(request);
+            var stream = response.Content.ReadAsStream();
+            StreamReader reader = new StreamReader(stream);
+            string json = reader.ReadToEnd();
+            return json;
+        }
     }
 
     public class APIResult
     {
         public int totalItems { get; set; }
         public APIResultRow[] items { get; set;}
+        public APIResult()
+        {
+            this.totalItems = 0;
+            this.items = new APIResultRow[0];
+        }
     }
 
     public class APIResultRow
@@ -23,5 +143,15 @@ namespace WinPharmaAPI
         public string description { get; set; }
         public string price { get; set; }
         public int stock { get; set; }
+
+        public APIResultRow()
+        {
+            code = "";
+            title = "";
+            manufacturer = "";
+            description = "";
+            price = "";
+            stock = 0;
+        }
     }
 }
--- WinPharmaAPI/Controllers/ApiRedirectionController.cs
+++ WinPharmaAPI/Controllers/ApiRedirectionController.cs
@@ -9,101 +9,20 @@ namespace WinPharmaAPI.Controllers
     [Route("[controller]")]
     public class ApiRedirectionController : Controller
     {
+        private readonly IConfiguration config;
         //Задаем кэш
         private IMemoryCache cache;
-        public ApiRedirectionController(IMemoryCache memoryCache)
+        public ApiRedirectionController(IMemoryCache memoryCache, IConfiguration _config)
         {
             cache = memoryCache;
+            config = _config;
         }
         [HttpGet("~/RedirectToAPI", Name = "RedirectToAPI")]
         public ActionResult RedirectToAPI(int? Skip, int? Take, string? Expand, string? Filter, string? OrderBy, string? OrderDirection)
         {
-            var url = "http://fakestock.everys.com/api/v1/Stock?";
-            //объявляем переменную, которая будет ключем в кэше
-            string addToUrl = "";
-            if (Skip != null)
-            {
-                addToUrl += "Skip=" + Skip.ToString();
-            }
-            if (Take != null)
-            {
-                addToUrl += (addToUrl != "" ? "&" : "") + "Take=" + Take.ToString();
-            }
-            if (Expand != null)
-            {
-                addToUrl += (addToUrl != "" ? "&" : "") + "Expand=" + Expand;
-            }
-            if (Filter != null)
-            {
-                addToUrl += (addToUrl != "" ? "&" : "") + "Filter=" + Filter;
-            }
-            if (OrderBy != null)
-            {
-                addToUrl += (addToUrl != "" ? "&" : "") + "OrderBy=" + OrderBy;
-            }
-            if (OrderDirection != null)
-            {
-                addToUrl += (addToUrl != "" ? "&" : "") + "OrderDirection=" + OrderDirection;
-            }
-            var res = new APIRequest();
-            try
-            {
-                var json = GetJsonFromUrl(url + addToUrl);
-                res = JsonConvert.DeserializeObject<APIRequest>(json);
-                //Если апи ответила нормально, но со статусом Error - берем из кэша
-                if (res.status == "Error")
-                {
-                    //Если в кэше не найдено - бросаем ошибку
-                    if (!cache.TryGetValue(addToUrl, out res))
-                    {
-                        throw new Exception("API overloaded and value not in cache");
-                    }
-                    //Если найдено - меняем статус
-                    else
-                    {
-                        res.status = "FromCache";
-                    }
-                }
-                //Добавляем в кэш по ключу и полученному res
-                else
-                {
-                    cache.Set(addToUrl, res);
-                }
-            }
-            //Отлавливаем непредвиденные ошибки, пытаемся взять значение из кэша, если не получается - кидаем ошибку
-            catch (Exception ex)
-            {
-                if (!cache.TryGetValue(addToUrl, out res))
-                {
-                    throw new Exception("Error occured ("+ex.Message+") and value not in cache");
-                }
-                else
-                {
-                    res.status = "FromCache";
-                }
-            }
+            var res = new APIRequest(Skip, Take, Expand, Filter, OrderBy, OrderDirection, cache, config);
 
             return Json(res);
-        }
-
-        public static string GetJsonFromUrl(string url)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get,
-            url);
-
-            var authenticationString = "candidate:candidate321";
-            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
-            request.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
-
-            //request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
-
-            var client = new HttpClient();
-
-            var response = client.Send(request);
-            var stream = response.Content.ReadAsStream();
-            StreamReader reader = new StreamReader(stream);
-            string json = reader.ReadToEnd();
-            return json;
         }
     }
 }
